17a519249e0d9bd20e333fd780599b25b4fa682d
monuyadav6284 <monu628493@gmail.com>
2024-07-19 17:51:36 +0530

    last commit

---
+++ Frontend/src/home/Leftpart/User.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import useConversation from "../../zustand/useConversation.js";
+import { useSocketContext } from "../../context/SocketContext.jsx";
+
+function User({ user }) {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  const isSelected = selectedConversation?._id === user._id;
+  const { socket, onlineUsers } = useSocketContext();
+  const isOnline = onlineUsers.includes(user._id);
+  return (
+    <div
+      className={`hover:bg-slate-600 duration-300 ${
+        isSelected ? "bg-slate-700" : ""
+      }`}
+      onClick={() => setSelectedConversation(user)}
+    >
+      <div className="flex space-x-4 px-8 py-3 hover:bg-slate-700 duration-300 cursor-pointer">
+        <div className={`avatar ${isOnline ? "online" : ""}`}>
+          <div className="w-12 rounded-full">
+            <img src="https://images.unsplash.com/photo-1713553589836-48fcc4b156a1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+          </div>
+        </div>
+        <div>
+          <h1 className=" font-bold">{user.fullname}</h1>
+          <span>{user.email}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default User;
---
+++ Frontend/src/home/Rightpart/Chatuser.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import useConversation from "../../zustand/useConversation.js";
+import { useSocketContext } from "../../context/SocketContext.jsx";
+import { CiMenuFries } from "react-icons/ci";
+
+function Chatuser() {
+  const { selectedConversation } = useConversation();
+  const { onlineUsers } = useSocketContext();
+  const getOnlineUsersStatus = (userId) => {
+    return onlineUsers.includes(userId) ? "Online" : "Offline";
+  };
+
+  // console.log(selectedConversation.fullname);
+  return (
+    <div className="relative flex items-center h-[8%] justify-center gap-4 bg-slate-800 hover:bg-slate-700 duration-300 rounded-md">
+      <label
+        htmlFor="my-drawer-2"
+        className="btn btn-ghost drawer-button lg:hidden absolute left-5"
+      >
+        <CiMenuFries className="text-white text-xl" />
+      </label>
+      <div className="flex space-x-3 items-center justify-center h-[8vh] bg-gray-800 hover:bg-gray-700 duration-300">
+        <div className="avatar online ">
+          <div className="w-12 rounded-full">
+            <img src="https://images.unsplash.com/photo-1713553589836-48fcc4b156a1?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+          </div>
+        </div>
+        <div>
+          <h1 className="text-xl">{selectedConversation.fullname}</h1>
+          <span className="text-sm">
+            {getOnlineUsersStatus(selectedConversation._id)}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Chatuser;
